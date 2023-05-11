f9f98806cf66ccf6f78bb37b3cfc9de2c40f146c
Juan Pablo <juan654.pablo@gmail.com>
2023-05-11 19:33:46 -0300

    Inicio do projeto Api-Livraria

---
+++ index.js
@@ -0,0 +1,33 @@
+/*IMPORTA O PACOTE DO EXPRESS PARA O SCRIPT index.js*/
+const express = require('express');
+
+/*TORNA O EXPRESS EXECUTÁVEL DENTRO DO SCRIPT inxdex.js
+ATRAVÉS DA CONSTANTE app
+*/
+const app = express();
+
+app.use(express.json());
+//app.use(express.urlencoded({extended:true}));
+
+// const router = express.Router();
+
+// app.post('/cadastrarLivro', (req, res)=>{
+//     console.log(req.body);
+//     res.send('teste');
+// });
+const routesCategoria = require('./route/routesCategorias');
+app.use('/', routesCategoria);
+
+const routesLivro = require('./route/routesLivros');
+app.use('/', routesLivro);
+
+/*
+CRIAR UM WEBSERVER CAPAZ DE RECEBER REQUISIÇÕES VIA
+HTTP
+PARAMETROS DO listem
+1 - PORTA LÓGICA (OBRIGATÓRIA)
+2 - CALLBACK (OPCIONAL)
+*/
+app.listen(3000, ()=>{
+    console.log('SERVIDOR RODANDO EM - http://localhost:3000');
+});
